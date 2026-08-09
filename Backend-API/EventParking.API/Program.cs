56eac978982fa190f2fef4887b9abfa9fb041357
Pragaszhyoag ChrisKlegarYoag <gphpyoag@gmail.com>
2026-08-09 11:37:56 +0530

    feat(booking): register booking dependencies

--- Backend-API/EventParking.API/Program.cs
+++ Backend-API/EventParking.API/Program.cs
@@ -26,6 +26,9 @@ builder.Services.AddScoped<ICustomerRepository, CustomerRepository>();
 builder.Services.AddScoped<AuthService>();
 builder.Services.AddScoped<CustomerService>();
 
+builder.Services.AddScoped<IBookingRepository, BookingRepository>();
+builder.Services.AddScoped<IBookingService, BookingService>();
+
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
